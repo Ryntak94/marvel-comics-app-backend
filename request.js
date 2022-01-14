e6c06ad8f54e8b4ae3d41b87af304cb4ecfc3d9e
ryntak94 <ryan.matthews.professional@gmail.com>
2022-01-14 13:35:57 -0600

    update .gitignore to ignore jsonFiles folder. Create file to algorithmically copy all responses from marvel api to json files

---
+++ request.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const md5 = require('md5')
+const request = require('request-promise');
+const fs = require('fs');
+const { default: forExport } = require('neo4j-driver-core');
+
+const uri = process.env.URI
+const user = process.env.USER
+const password = process.env.PASSWORD
+
+const timestamp = Date.now()
+const options = {
+    uri: "http://gateway.marvel.com/v1/public/comics",
+    method: "GET",
+    qs: {
+        ts: timestamp,
+        apikey: process.env.PUBLICKEY,
+        hash: md5(timestamp+process.env.PRIVATEKEY+process.env.PUBLICKEY),
+        limit: 100,
+        offset: 0
+    },
+    json: true,
+}
+
+const requestRecurse = (settings    =>  {
+    request(settings)
+    .then((res)   =>  {
+        let data = JSON.stringify(res.data.results)
+        let offset = settings.qs.offset
+
+        let tenTInc = Math.floor(offset / 10000)*10000
+        let oneTInc = Math.floor(offset / 1000)*1000
+    
+        let parent = `${tenTInc+1} - ${tenTInc + 10000}`
+        let child = `${oneTInc+1} - ${oneTInc+1000}`
+        let file = `${offset+1}-${offset+100}.json`
+        
+        fs.existsSync('jsonFiles') ?  null : fs.mkdirSync('jsonFiles')
+        fs.existsSync(`jsonFiles/${parent}`)   ? null : fs.mkdirSync(`jsonFiles/${parent}`)
+        fs.existsSync(`jsonFiles/${parent}/${child}`) ? null : fs.mkdirSync(`jsonFiles/${parent}/${child}`)
+        fs.writeFileSync(`jsonFiles/${parent}/${child}/${file}`, data)
+        console.log(res)
+        if(res.data.total >= offset) {
+            let newSettings = settings
+            newSettings.qs.offset = offset+100
+            requestRecurse(newSettings)
+        }   else    {
+            return console.log('fin.')
+        }
+        
+    })
+    .catch(()   =>  {
+        requestRecurse(settings)
+    })
+})
+
+requestRecurse(options)
+// const offset = options.qs.offset
+
+// let tenTInc = Math.floor(offset / 10000)*10000
+// let oneTInc = Math.floor(offset / 1000)*1000
+
+// let parent = `${tenTInc+1} - ${tenTInc + 10000}`
+// let child = `${oneTInc+1} - ${oneTInc+1000}`
+
+// console.log(tenTInc, oneTInc)
+// console.log(parent, child)
+    
+    // console.log(file, child, parent)
+
+
